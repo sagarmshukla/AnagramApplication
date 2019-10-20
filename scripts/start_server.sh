73a2b89225d2e7304eca6696abea755a3a891c33
Sagar Shukla <Sagar.Shukla@insurity.com>
2019-10-20 10:42:18 -0400

    Updated dependences
    Signed-off-by: Sagar Shukla

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 #service httpd start
+
+sudo cp -a /var/www/html/AnagramApplication2/vendor/ /var/www/html/AnagramApplication
+cd /var/www/html/AnagramApplication
+sudo chown -R $USER:www-data storage
+sudo chown -R $USER:www-data bootstrap/cache
+chmod -R 775 storage
+chmod -R 775 bootstrap/cache
