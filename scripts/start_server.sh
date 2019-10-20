052287ca0f9e9940b7118d44ec0d39f52b5878bf
Sagar Shukla <Sagar.Shukla@insurity.com>
2019-10-20 10:50:14 -0400

    Added Session folder
    Signed-off-by: Sagar Shukla

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,6 +2,7 @@
 #service httpd start
 
 sudo cp -a /var/www/html/AnagramApplication2/vendor/ /var/www/html/AnagramApplication
+sudo cp -a /var/www/html/AnagramApplication2/.env /var/www/html/AnagramApplication
 cd /var/www/html/AnagramApplication
 sudo chown -R $USER:www-data storage
 sudo chown -R $USER:www-data bootstrap/cache
